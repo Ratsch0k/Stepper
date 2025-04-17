e36e9ba485fb9c1ffe07bb07e721d10314a9b4df
Simon Kurz <simonlukas.98@gmail.com>
2025-04-17 13:52:41 -0400

    Removed debug printing in message processor

--- src/main/java/com/coreyd97/stepper/MessageProcessor.java
+++ src/main/java/com/coreyd97/stepper/MessageProcessor.java
@@ -3,7 +3,6 @@ package com.coreyd97.stepper;
 import burp.*;
 
 import com.coreyd97.BurpExtenderUtilities.Preferences;
-import com.coreyd97.stepper.sequence.StepSequence;
 import com.coreyd97.stepper.sequence.StepSequenceState;
 import com.coreyd97.stepper.sequence.view.StepSequenceTab;
 import com.coreyd97.stepper.sequencemanager.SequenceManager;
@@ -322,14 +321,12 @@ public class MessageProcessor implements IHttpListener {
      * @return Map of all sequence arguments
      */
     private Map<String, String> extractSequenceArgumentsFromRequest(IRequestInfo requestInfo, Pattern pattern) {
-        Stepper.callbacks.printOutput("[MessageProcessor] extract arguments");        
         //Check if headers ask us to execute a request before the request.
         List<String> requestHeaders = requestInfo.getHeaders();
         Map<String, String> arguments = new HashMap<>();
 
         for (Iterator<String> iterator = requestHeaders.iterator(); iterator.hasNext(); ) {
             String header = iterator.next();
-            Stepper.callbacks.printOutput("[MessageProcessor] processing header: " + header);
 
             Matcher m = pattern.matcher(header);
             if (!m.matches()) {
@@ -338,18 +335,13 @@ public class MessageProcessor implements IHttpListener {
 
             String variableInfo = m.group(1).trim();
 
-            Stepper.callbacks.printOutput("[MessageProcessor] processing variable info: " + variableInfo);
-
             Matcher argumentMatcher = SINGLE_VARIABLE_PATTERN.matcher(variableInfo);
             if (!argumentMatcher.matches()) {
-                Stepper.callbacks.printOutput("[MessageProcessor] found variable header without value");
                 continue;
             }
 
             String variableKey = argumentMatcher.group("key");
             String variableValue = argumentMatcher.group("value");
-
-            Stepper.callbacks.printOutput("[MessageProcessor] got argument: " + variableKey + "=" + variableValue);
 
             arguments.put(variableKey, variableValue);
         }
